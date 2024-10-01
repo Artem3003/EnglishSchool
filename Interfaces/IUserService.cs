b69e1087d50243c1ab54072a7c0c266627193f6a
Artem <temapro04@gmail.com>
2024-10-01 12:23:21 +0300

    Interfaces created

---
+++ Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using demo_english_school.Models;
+
+namespace demo_english_school.Interfaces;
+
+public interface IUserService : ICrud<User>
+{
+    
+}
